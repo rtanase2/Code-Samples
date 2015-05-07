d23d2292027b1d468c59127e7d2da6a3ebc61ce6
wc_karatekid <rtanase@ucsc.edu>
2015-05-07 11:09:28 -0700

    Finished main function

--- 2015-05-06-Challenge-213-Intermediate-The-Lazy-Typist/lazy_typist.py
+++ 2015-05-06-Challenge-213-Intermediate-The-Lazy-Typist/lazy_typist.py
@@ -8,8 +8,12 @@ def find_effort_greedy(s):
 
 def main():
     """Takes in user iput and returns the moves and total effort for each."""
+    print ("Welcome to my Lazy Typist simulator!\n" + 
+           "You can exit at any time by typing \"quit\".")
     while (True):
         s = raw_input("Please enter the string to type: ")
+        if (s == "quit"):
+            break
         print "The lazy way to type \"{0}\" using a greedy algorithm is:\n{1}"\
               .format(s, find_effort_greedy(s))
 
