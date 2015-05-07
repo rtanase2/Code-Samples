2d0ff4d078110660e61019109000152319f1f9c5
wc_karatekid <rtanase@ucsc.edu>
2015-05-07 15:57:55 -0700

    Implemented small inputs
    Handles case of string with one capital letter, two lower case letters,
    or an empty string

--- 2015-05-06-Challenge-213-Intermediate-The-Lazy-Typist/lazy_typist.py
+++ 2015-05-06-Challenge-213-Intermediate-The-Lazy-Typist/lazy_typist.py
@@ -2,9 +2,35 @@ __author__ = 'Rachelle Tanase'
 
 def find_effort_greedy(s):
     """Return the moves and total effort for tying the string, s."""
-    total_effort = 0
     if len(s) == 0:
         return "Total effort: 0"
+    else:
+        result = []
+        total_effort = 0
+        keyboard = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+                    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '-'],
+                    ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
+                    ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
+        for c in s[placeHands(s, result):len(s)]:
+            if(c.isupper()):
+                nextChars = [c, "^"]
+        result.append("Total effort: {0}".format(total_effort))
+        return "\n".join(result)
+
+def placeHands(s, r):
+    """Places hands in first position"""
+    if (s[0].isupper()):
+        r.append("Shift: Use left hand")
+        r.append("{0}: Use right hand".format(s[0].upper()))
+        return 1
+    elif (len(s) == 1):
+        r.append("{0}: Use left hand".format(s[0].upper()))
+        return 1
+    else:
+        r.append("{0}: Use left hand".format(s[0].upper()))
+        r.append("{0}: Use right hand".format(s[1].upper()))
+        return 2
+
 
 def main():
     """Takes in user iput and returns the moves and total effort for each."""
