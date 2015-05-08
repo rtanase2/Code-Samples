9a38afc38bd686dbdad320bb2dfe5896bf3d7d69
wc_karatekid <rtanase@ucsc.edu>
2015-05-07 18:20:00 -0700

    Added functions and began final implementation
    Can find Manhattan distance and send the correct name of a space e.g.
    instead of printing #, it prints Space.

--- 2015-05-06-Challenge-213-Intermediate-The-Lazy-Typist/lazy_typist.py
+++ 2015-05-06-Challenge-213-Intermediate-The-Lazy-Typist/lazy_typist.py
@@ -3,7 +3,7 @@ __author__ = 'Rachelle Tanase'
 def find_effort_greedy(s):
     """Return the moves and total effort for tying the string, s.
     
-    Keyword arguements:
+    Keyword arguments:
     s -- string to type
 
     Precondition:
@@ -19,8 +19,25 @@ def find_effort_greedy(s):
                     ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
                     ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
         start, left_pos, right_pos = place_hands(s, result, keyboard)
+        shift_positions = [(2, 0), (2, 9)]
         for c in s[start:len(s)]:
             if(c.isupper()):
+                left_dist_to_shift = 999999999999
+                right_dist_to_shift = 999999999999
+                for shift_pos in shift_positions:
+                    left_dist = find_dist(left_pos, shift_pos)
+                    right_dist = find_dist(right_pos, shift_pos)
+                    if left_dist < left_dist_to_shift:
+                        left_dist_to_shift = left_dist
+                    if right_dist < right_dist_to_shift:
+                        right_dist_to_shift = right_dist
+                if (min (left_dist_to_shift, right_dist_to_shift) == left_dist_to_shift):
+                    # Move left hand to shift and move right to letter
+                    result.append("Shift: Move left hand from {0}".format(name_of_pos(left_pos, keyboard)))
+                else:
+                    # Move right hand to shift and move left hand to letter
+                    pass
+            else:
                 pass
         result.append("Total effort: {0}".format(total_effort))
         return "\n".join(result)
@@ -30,21 +47,50 @@ def place_hands(s, r, kb):
     if (s[0].isupper()):
         r.append("Shift: Use left hand")
         r.append("{0}: Use right hand".format(s[0].upper()))
-        return 1, (2, 0), find_hand_pos(s[0], kb)
+        return 1, (2, 0), find_char_pos(s[0], kb)
     elif (len(s) == 1):
         r.append("{0}: Use left hand".format(s[0].upper()))
         return 1, (0, 0), (0, 0)
     else:
         r.append("{0}: Use left hand".format(s[0].upper()))
         r.append("{0}: Use right hand".format(s[1].upper()))
-        return 2, find_hand_pos(s[0], kb), find_hand_pos(s[1], kb)
+        return 2, find_char_pos(s[0], kb), find_char_pos(s[1], kb)
 
-def find_hand_pos(c, kb):
+def find_char_pos(c, kb):
     """Find returns tuple with hand position, given hand is over c on kb"""
     c = c.upper()
     for x in range(0, len(kb)):
         if c in kb[x]:
             return (x, kb[x].index(c))
+
+def find_dist(coords1, coords2):
+    """Returns the Manhattan distance between coords1 and coords2
+
+    Keyword arguments:
+    coords1 -- Tuple of starting position
+    coords2 -- Tuple of ending position
+
+    Precondition:
+    Assert coords1 and coords2 are tuples
+
+    Extra Info:
+    Manhattan distance = abs(x1 - x2) + abs(y1 - y2)
+    """
+    x_diff = abs(coords1[0] - coords2[0])
+    y_diff = abs(coords1[1] - coords2[1])
+    return (x_diff + y_diff)
+
+def name_of_pos(coord, kb):
+    """Returns the correct name for the position of coord on kb, 
+    especially for space and shift.
+    """
+    coord_char = kb[coord[0]][coord[1]]
+    if coord_char == '^':
+        return "Shift"
+    elif coord_char == '#':
+        return "Space"
+    else:
+        return coord_char
 
 def main():
     """Takes in user iput and returns the moves and total effort for each."""
@@ -56,7 +102,6 @@ def main():
             break
         print "The lazy way to type \"{0}\" using a greedy algorithm is:\n{1}"\
               .format(s, find_effort_greedy(s))
-
 
 if __name__ == '__main__':
     main()
--- 2015-05-06-Challenge-213-Intermediate-The-Lazy-Typist/lazy_typist_test.py
+++ 2015-05-06-Challenge-213-Intermediate-The-Lazy-Typist/lazy_typist_test.py
@@ -1,5 +1,5 @@
 import unittest
-from lazy_typist import find_effort_greedy, place_hands, find_hand_pos
+import lazy_typist
 
 __author__ = 'Rachelle Tanase'
 
@@ -7,47 +7,47 @@ class find_effort_greedy_tests(unittest.TestCase):
     '''Perform unit tests for the find_effort_greedy function'''
     def test_empty_string(self):
         self.assertEqual(
-            find_effort_greedy(""), 
+            lazy_typist.find_effort_greedy(""), 
             "Total effort: 0")
 
     def test_string_len_1(self):
         self.assertEqual(
-            find_effort_greedy("a"), 
+            lazy_typist.find_effort_greedy("a"), 
             "A: Use left hand\nTotal effort: 0")
 
     def test_string_len_2(self):
         self.assertEqual(
-            find_effort_greedy("ab"),
+            lazy_typist.find_effort_greedy("ab"),
             "A: Use left hand\nB: Use right hand\nTotal effort: 0")
 
     def test_upper_case_letter(self):
         self.assertEqual(
-            find_effort_greedy("T"), 
+            lazy_typist.find_effort_greedy("T"), 
             "Shift: Use left hand\nT: Use right hand\nTotal effort: 0")
 
     def test_sample_string_1(self):
         self.assertEqual(
-            find_effort_greedy("The quick brown Fox"),
+            lazy_typist.find_effort_greedy("The quick brown Fox"),
             "Shift: Use left hand\nT: Use right hand\nH: Move right hand from T (effort: 2)\nE: Move left hand from Shift (effort: 4)\nSpace: Move right hand from H (effort: 2)\nQ: Move left hand from E (effort: 2)\nU: Move right hand from Space (effort: 4)\nI: Move right hand from U (effort: 1)\nC: Move left hand from Q (effort: 5)\nK: Move right hand from I (effort: 1)\nSpace: Move left hand from C (effort: 1)\nB: Move left hand from Space (effort: 3)\nR: Move left hand from B (effort: 4)\nO: Move right hand from K (effort: 2)\nW: Move left hand from R (effort: 2)\nN: Move right hand from O (effort: 4)\nSpace: Move right hand from N (effort: 1)\nShift: Move left hand from W (effort: 3)\nF: Move right hand from Space (effort: 5)\nO: Move right hand from F (effort: 6)\nX: Move left hand from Shift (effort: 2)\nTotal effort: 54")
 
     def test_sample_string_2(self):
         self.assertEqual(
-            find_effort_greedy("hello world"),
+            lazy_typist.find_effort_greedy("hello world"),
             "H: Use left hand\nE: Use right hand\nL: Move left hand from H (effort: 3)\nL: Use left hand again\nO: Move left hand from L (effort: 1)\n\Space: Move left hand from O (effort: 4)\nW: Move right hand from E (effort: 1)\nO: Move left hand from Space (effort: 4)\nR: Move right hand from W (effort: 2)\nL: Move left hand from O (effort: 1)\nD: Move right hand from R (effort: 2)\nTotal effort: 18")
 
     def test_sample_string_3(self):
         self.assertEqual(
-            find_effort_greedy("qpalzm woskxn"),
+            lazy_typist.find_effort_greedy("qpalzm woskxn"),
             "Q: Use left hand\nP: Use right hand\nA: Move left hand from Q (effort: 1)\nL: Move right hand from P (effort: 2)\nZ: Move left hand from A (effort: 2)\nM: Move right hand from L (effort: 2)\nSpace: Move right hand from M (effort: 1)\nW: Move left hand from Z (effort: 2)\nO: Move right hand from Space (effort: 4)\nS: Move left hand from W (effort: 1)\nK: Move right hand from O (effort: 2)\nX: Move left hand from S (effort: 2)\nN: Move right hand from K (effort: 2)\nTotal effort: 21")
 
     def test_sample_string_4(self):
         self.assertEqual(
-            find_effort_greedy("Hello there DailyProgrammers"),
+            lazy_typist.find_effort_greedy("Hello there DailyProgrammers"),
             "Shift: Use left hand\nH: Use right hand\nE: Move left hand from Shift (effort: 4)\nL: Move right hand from H (effort: 3)\nL: Use right hand again\nO: Move right hand from L (effort: 1)\nSpace: Move left hand from E (effort: 4)\nT: Move left hand from Space (effort: 4)\nH: Move left hand from T (effort: 2)\nE: Move left hand from H (effort: 4)\nR: Move left hand from E (effort: 1)\nE: Move left hand from R (effort: 1)\nSpace: Move left hand from E (effort: 4)\nShift: Move right hand from O (effort: 3)\nD: Move left hand from Space (effort: 3)\nA: Move left hand from D (effort: 2)\nI: Move right hand from Shift (effort: 4)\nL: Move right hand from I (effort: 2)\nY: Move right hand from L (effort: 4)\nShift: Move left hand from A (effort: 1)\nP: Move right hand from Y (effort: 4)\nR: Move left hand from Shift (effort: 5)\nO: Move right hand from P (effort: 1)\nG: Move left hand from R (effort: 2)\nR: Move left hand from G (effort: 2)\nA: Move left hand from R (effort: 4)\nM: Move right hand from O (effort: 3)\nM: Use right hand again\nE: Move left hand from A (effort: 3)\nR: Move left hand from E (effort: 1)\nS: Move left hand from R (effort: 3)\nTotal effort: 75")
 
     def test_sample_string_5(self):
         self.assertEqual(
-            find_effort_greedy("Ty"), 
+            lazy_typist.find_effort_greedy("Ty"), 
             "Shift: Use left hand\nT: Use right hand\nY: Move right hand from T (effort: 1)\nTotal effort: 1")
 
 
@@ -59,7 +59,7 @@ class place_hands_tests(unittest.TestCase):
                     ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
                     ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
         r = []
-        place_hands("a", r, keyboard)
+        lazy_typist.place_hands("a", r, keyboard)
         self.assertEqual(r, ["A: Use left hand"])
 
     def test_string_len_1_upper_case(self):
@@ -68,7 +68,7 @@ class place_hands_tests(unittest.TestCase):
                     ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
                     ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
         r = []
-        place_hands("A", r, keyboard)
+        lazy_typist.place_hands("A", r, keyboard)
         self.assertEqual(r, ["Shift: Use left hand", 
                              "A: Use right hand"])
 
@@ -78,48 +78,84 @@ class place_hands_tests(unittest.TestCase):
                     ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
                     ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
         r = []
-        place_hands("ab", r, keyboard)
+        lazy_typist.place_hands("ab", r, keyboard)
         self.assertEqual(r, ["A: Use left hand", 
                              "B: Use right hand"])
 
 
-class find_hand_pos_tests(unittest.TestCase):
-    '''Perform unit tests for the find_hand_pos function'''
+class find_char_pos_tests(unittest.TestCase):
+    '''Perform unit tests for the find_char_pos function'''
     def test_shift(self):
         keyboard = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
                     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '-'],
                     ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
                     ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
-        self.assertEqual(find_hand_pos('^', keyboard), (2, 0))
+        self.assertEqual(lazy_typist.find_char_pos('^', keyboard), (2, 0))
 
     def test_space(self):
         keyboard = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
                     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '-'],
                     ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
                     ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
-        self.assertEqual(find_hand_pos('#', keyboard), (3, 3))
+        self.assertEqual(lazy_typist.find_char_pos('#', keyboard), (3, 3))
 
     def test_a(self):
         keyboard = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
                     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '-'],
                     ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
                     ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
-        self.assertEqual(find_hand_pos('a', keyboard), (1, 0))
+        self.assertEqual(lazy_typist.find_char_pos('a', keyboard), (1, 0))
 
     def test_v(self):
         keyboard = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
                     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '-'],
                     ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
                     ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
-        self.assertEqual(find_hand_pos('v', keyboard), (2, 4))
+        self.assertEqual(lazy_typist.find_char_pos('v', keyboard), (2, 4))
 
+class find_dist_tests(unittest.TestCase):
+    '''Perform unit tests for the find_dist function'''
+    def test_same_coords(self):
+        self.assertEqual(lazy_typist.find_dist((0, 0), (0, 0)), 0)
+
+    def test_case_1(self):
+        self.assertEqual(lazy_typist.find_dist((0, 0), (1, 1)), 2)
+
+    def test_case_2(self):
+        self.assertEqual(lazy_typist.find_dist((2, 3), (-6, -1)), 12)
+
+class name_of_pos_tests(unittest.TestCase):
+    '''Perform unit tests for the name_of_pos function'''
+    def test_space(self):
+        keyboard = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+                    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '-'],
+                    ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
+                    ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
+        self.assertEqual(lazy_typist.name_of_pos((3, 3), keyboard), "Space")
+
+    def test_shift(self):
+        keyboard = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+                    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '-'],
+                    ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
+                    ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
+        self.assertEqual(lazy_typist.name_of_pos((2, 0), keyboard), "Shift")
+
+    def test_char(self):
+        keyboard = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+                    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '-'],
+                    ['^', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', '^'],
+                    ['-', '-', '-', '#', '#', '#', '#', '#', '-', '-']]
+        self.assertEqual(lazy_typist.name_of_pos((2, 6), keyboard), "N")
 
 def main():
     """Runs all test cases"""
     # Contains all names of test categories (classes) in string form
-    test_class_names = ["find_effort_greedy_tests", "place_hands_tests", "find_hand_pos_tests"]
+    test_class_names = ["find_effort_greedy_tests", "place_hands_tests", 
+                        "find_char_pos_tests", "find_dist_tests", 
+                        "name_of_pos_tests"]
     # Contains all classes with test cases
-    tests = [find_effort_greedy_tests, place_hands_tests, find_hand_pos_tests]
+    tests = [find_effort_greedy_tests, place_hands_tests, find_char_pos_tests,
+             find_dist_tests, name_of_pos_tests]
     # Parses through all test classes and and prints them out
     for index in range(0, len(tests)):
         print "\n\n###### Running {0} ######\n".format(test_class_names[index])
