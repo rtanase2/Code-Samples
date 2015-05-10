87f244956748e64f1406c04aa141a7e83c742642
wc_karatekid <rtanase@ucsc.edu>
2015-05-09 18:44:32 -0700

    Have treats sorted and printing properly
    Just need to calculate the happiness

--- Java/Hungry-Puppies [Hard]/HungryPuppies.java
+++ Java/Hungry-Puppies [Hard]/HungryPuppies.java
@@ -17,18 +17,152 @@ public class HungryPuppies {
             if(line.equals("quit")){
                 break;
             } else {
-                findMaxHappiness(line);
+                String[] results = findBestOrder(line);
+                System.out.println(results[0]);
+                System.out.println(addSpaces(results[1].split("")) + "\n");
             }
         }    
     }
 
-    public static void findMaxHappiness(String s){
+    private static String addSpaces(String[] a){
+        String results = "";
+        for (String s : a){
+            results += s + " ";
+        }
+        return results;
+    }
+
+    public static String[] findBestOrder(String s){
         HashMap<Integer, Integer> treatCount = new HashMap<Integer, Integer> ();
         String[] temp = s.split(" ");
         for(int i = 0; i < temp.length; i++){
             addToHashMap(treatCount, Integer.parseInt(temp[i]));
         }
-        System.out.println(treatCount);
+        String[] ret = new String[2];
+        String result = createHLLHPattern(treatCount);
+        while (true){
+            if(treatCount.size() >= 2 && hasAtLeast2OfASize(treatCount)){
+                result = addAnotherHLLH(treatCount, result);
+            } 
+            else if (treatCount.isEmpty()){
+                //if there are no treats left, you are done!
+                ret[1] = result;
+                break;
+            }
+            else {
+                //has any number of elems, but only one of each size
+                ret[1] = sortRemainingTreats(treatCount, result);
+                break;
+            }
+        }
+        ret[0] = "Calculate the happiness!";
+        return(ret);
+    }
+
+    public static String sortRemainingTreats(HashMap<Integer, Integer> hm, String curr){
+        int[] used = new int[hm.size()];
+        int index = 0;
+        for (int k : hm.keySet()){
+            System.out.println(curr);
+            System.out.println(k);
+            int leftEdge = Integer.parseInt(Character.toString(curr.charAt(0)));
+            int rightEdge = Integer.parseInt(Character.toString(curr.charAt(curr.length() - 1)));
+            Boolean actionDone = false;
+            if (k >= rightEdge){
+                for(int j : hm.keySet()){
+                    if(j < rightEdge && !in(used, j)){
+                        //then it is less than k and forms "sandwich" (aka High-Low-High)
+                        //ex: 324 is accepted
+                        curr += Integer.toString(j) + Integer.toString(k);
+                        used[index++] = j;
+                        used[index++] = k;
+                        actionDone = true;
+                        break;
+                    }
+                }
+                if (!actionDone){
+                    curr += k;
+                    used[index++] = k;
+                }
+                rightEdge = k;
+            }
+            else if (k >= leftEdge){
+                for(int j : hm.keySet()){
+                    if(j < leftEdge && !in(used, j)){
+                        //then it is less than k and forms "sandwich" (aka High-Low-High)
+                        //ex: 324 is accepted
+                        curr = k + j + curr;
+                        used[index++] = j;
+                        used[index++] = k;
+                        actionDone = true;
+                        break;
+                    }
+                }
+                if (!actionDone){
+                    curr = k + curr; 
+                    used[index++] = k; 
+                }
+                leftEdge = k;
+            }
+            actionDone = false;
+        }
+        System.out.println(curr);
+        return(curr);
+    }
+
+    public static Boolean in(int[] a, int x){
+        for (int i : a){
+            if(x==i){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Boolean hasAtLeast2OfASize(HashMap<Integer, Integer> hm){
+        for (int key : hm.keySet()){
+            if (hm.get(key) > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String addAnotherHLLH(HashMap<Integer, Integer> hm, String curr){
+        int leftEdge = Integer.parseInt(Character.toString(curr.charAt(0)));
+        int rightEdge = Integer.parseInt(Character.toString(curr.charAt(curr.length() - 1)));
+        int middleElem = 0; 
+        int middleSize = 0;
+        System.out.println(leftEdge);
+        System.out.println(rightEdge);
+
+        for (int key : hm.keySet()){
+            if(hm.get(key) >= 2){
+                if (key < leftEdge || key < rightEdge){
+                    middleElem = key;
+                    if(hm.get(key)%2 == 1){
+                        middleSize = 3;
+                    } else {
+                        middleSize = 2;
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (int key : hm.keySet()){
+            if(key > middleElem){
+                if(middleElem < rightEdge){
+                    curr += repeat(middleElem, middleSize) + key;
+                } else {
+                    curr = key + repeat(middleElem, middleSize) + curr;
+                }
+                reduceCountInHashMap(hm, key, 1);
+                reduceCountInHashMap(hm, middleElem, middleSize);
+                break;
+            }
+        }
+        return curr;
     }
 
     public static void addToHashMap(HashMap<Integer, Integer> hm, int x){
@@ -45,9 +179,45 @@ public class HungryPuppies {
         hm.put(x, ++temp);
     }
     
-    public static void createHLLHPattern(HashMap<Integer, Integer> treatCount) {
-        for(int key : treatCount.keySet()) {
-            System.out.println(key);
+    public static String createHLLHPattern(HashMap<Integer, Integer> hm) {
+        //Precondition, there are at least 2 numbers with 2 or more elements
+        int highest = 0;
+        int secondHighest = 0;
+        int middleSize = 0;
+        for(int key : hm.keySet()) {
+            if(hm.get(key) >= 2){
+                if (key > highest){
+                    secondHighest = highest;
+                    highest = key;
+                } 
+                else if (key > secondHighest){
+                    secondHighest = key;
+                }
+            }
         }
+        if (hm.get(secondHighest) % 2 == 1){
+            middleSize = 3;
+        } else {
+            middleSize = 2;
+        }
+        reduceCountInHashMap(hm, highest, 2);
+        reduceCountInHashMap(hm, secondHighest, middleSize);
+        return (highest + repeat(secondHighest, middleSize) + highest);
+    }
+
+    public static void reduceCountInHashMap(HashMap<Integer, Integer> hm, int h, int size){
+        int temp = hm.get(h);
+        hm.remove(h);
+        if(temp-size > 0){
+            hm.put(h, temp-size);
+        }
+    }
+
+    private static String repeat(int x, int times){
+        String result = "";
+        for (int i = 0; i < times; i++) {
+            result += x;
+        }
+        return result;
     }
 }
--- Java/Hungry-Puppies [Hard]/HungryPuppiesTests.java
+++ Java/Hungry-Puppies [Hard]/HungryPuppiesTests.java
@@ -17,17 +17,134 @@ public class HungryPuppiesTests {
         System.out.println("--------------------------------------");
         testIncVal();   
         System.out.println("--------------------------------------\n");
+
+        System.out.println("\nRunning reduceCountInHashMap tests...");
+        System.out.println("--------------------------------------");
+        testReduceCountInHashMap();   
+        System.out.println("--------------------------------------\n");
+
+        System.out.println("\nRunning findCreateHLLHPattern tests...");
+        System.out.println("--------------------------------------");
+        testCreateHLLHPattern();   
+        System.out.println("--------------------------------------\n");
+
+        System.out.println("\nRunning addAnotherHLLH tests...");
+        System.out.println("--------------------------------------");
+        testAddAnotherHLLH();   
+        System.out.println("--------------------------------------\n");
+
+        System.out.println("\nRunning findBestOrder tests...");
+        System.out.println("--------------------------------------");
+        testFindBestOrder();   
+        System.out.println("--------------------------------------\n");
+        
+    }
+
+    private static void testAddAnotherHLLH(){
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer> ();
+        String temp = "5335";
+        int numTests = 2;
+        int numSuccesses = 0;
+
+        System.out.println("Attmpting to create another HLLH with 2 pairs in HashMap... ");
+        hm.put(4, 2);
+        hm.put(1, 2);
+        temp = HungryPuppies.addAnotherHLLH(hm, temp);
+        numSuccesses += printStatus(temp.equals("5335114") && hm.size() == 1);
+
+        System.out.println("Attempting to create HLLH pattern one pair in HashMap... ");
+        hm = new HashMap<Integer, Integer> ();
+        hm.put(1, 2);
+        hm.put(6, 1);
+        temp = HungryPuppies.addAnotherHLLH(hm, temp);
+        numSuccesses += printStatus(temp.equals("5335114116") && hm.size() == 0);
+
+        printResult(numSuccesses, numTests);
+    }
+
+    private static void testCreateHLLHPattern(){
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer> ();
+        String temp;
+        int numTests = 4;
+        int numSuccesses = 0;
+
+        System.out.println("Attempting to create HLLH pattern with odd lower values... ");
+        hm.put(3, 2);
+        hm.put(2, 3);
+        temp = HungryPuppies.createHLLHPattern(hm);
+        numSuccesses += printStatus(temp.equals("32223"));
+
+        System.out.println("Attempting to create HLLH pattern with odd higher values... ");
+        hm = new HashMap<Integer, Integer> ();
+        hm.put(3, 3);
+        hm.put(2, 2);
+        temp = HungryPuppies.createHLLHPattern(hm);
+        numSuccesses += printStatus(temp.equals("3223"));
+        
+        System.out.println("Attempting to create HLLH pattern with both even values... ");
+        hm = new HashMap<Integer, Integer> ();
+        hm.put(3, 2);
+        hm.put(2, 2);
+        temp = HungryPuppies.createHLLHPattern(hm);
+        numSuccesses += printStatus(temp.equals("3223"));
+
+        System.out.println("Attempting to create HLLH pattern with both odd values... ");
+        hm = new HashMap<Integer, Integer> ();
+        hm.put(3, 3);
+        hm.put(2, 3);
+        temp = HungryPuppies.createHLLHPattern(hm);
+        numSuccesses += printStatus(temp.equals("32223"));
+
+        printResult(numSuccesses, numTests);
     }
     
+    private static void testFindBestOrder(){
+        String[] temp = new String[2];
+        int numTests = 3;
+        int numSuccesses = 0;
+
+        System.out.println("Attempting to find maximum happiness of 1 1 1 1 1 2 2 3... ");
+        temp = HungryPuppies.findBestOrder("1 1 1 1 1 2 2 3");
+        numSuccesses += printStatus(temp[0] == "3" && temp[1] == "21112113");
+
+        System.out.println("Attempting to find maximum happiness of 1 2 2 3 3 3 4... ");
+        temp = HungryPuppies.findBestOrder("1 2 2 3 3 3 4");
+        numSuccesses += printStatus(temp[0] == "2" && temp[1] == "3223134");
+
+        System.out.println("Attempting to find maximum happiness of 1 1 2 3 3 3 3 4 5 5... ");
+        temp = HungryPuppies.findBestOrder("1 1 2 3 3 3 3 4 5 5");
+        numSuccesses += printStatus(temp[0] == "4" && temp[1] == "4335335112");
+
+        printResult(numSuccesses, numTests);
+    }
+
     private static void testIncVal(){
         HashMap<Integer, Integer> hm = new HashMap<Integer, Integer> ();
         int numTests = 1;
         int numSuccesses = 0;
 
-        System.out.print("Attempt to increment an existing element... ");
+        System.out.print("Attempting to increment an existing element... ");
         hm.put(1, 2);
         HungryPuppies.incVal(hm, 1);
         numSuccesses += printStatus(hm.get(1) == 3);
+
+        printResult(numSuccesses, numTests);
+    }
+
+    private static void testReduceCountInHashMap(){
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer> ();
+        int numTests = 2;
+        int numSuccesses = 0;
+
+        System.out.print("Attempting to decrease an element to zero count... ");
+        hm.put(1, 2);
+        HungryPuppies.reduceCountInHashMap(hm, 1, 2);
+        numSuccesses += printStatus(hm.get(1) == null);
+
+        System.out.print("Attempting to decrease an element to non-zero count... ");
+        hm.put(1, 3);
+        HungryPuppies.reduceCountInHashMap(hm, 1, 2);
+        numSuccesses += printStatus(hm.get(1) == 1);
 
         printResult(numSuccesses, numTests);
     }
